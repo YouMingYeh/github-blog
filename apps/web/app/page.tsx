1a1a71f0c4eb4c2b811d546aa169bff57c98c007
YouMingYeh <b10705052@ntu.edu.tw>
2024-02-12 02:03:27 +0800

    Add heading to page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -72,6 +72,7 @@ export default function Page() {
 
   return (
     <div className="relative z-0 p-3">
+      <h1 className="text-center text-3xl font-bold">Posts</h1>
       <HoverEffect
         items={issues.map((issue) => {
           ("use server");
