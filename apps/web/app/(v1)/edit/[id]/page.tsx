a1de723e8b78f47c1f3f594911d662eb59bb07e5
YouMingYeh <b10705052@ntu.edu.tw>
2024-02-13 15:35:05 +0800

    Add CustomEditor component

--- apps/web/app/(v1)/edit/[id]/page.tsx
+++ apps/web/app/(v1)/edit/[id]/page.tsx
@@ -57,6 +57,8 @@ import Link from "next/link";
 import LoadingCircle from "@/components/ui/icons/loading-circle";
 import IssueComments from "@/components/IssueComments";
 import { markdownToHtml } from "@/lib/converter";
+import CustomEditor from "@/components/CustomEditor";
+import TitleEditor from "@/components/TitleEditor";
 
 export default function Page() {
   const { id }: { id: string } = useParams();
@@ -73,12 +75,6 @@ export default function Page() {
   // UI State
   const [loading, setLoading] = useState(true);
   const [autoSaveInterval, setAutoSaveInterval] = useState(60);
-
-  // Feature Toggle State
-  const [openNode, setOpenNode] = useState(false);
-  const [openColor, setOpenColor] = useState(false);
-  const [openLink, setOpenLink] = useState(false);
-  const [openAI, setOpenAI] = useState(false);
 
   // Fetch and update issue content
   const updateRemote = useCallback(async () => {
@@ -211,98 +207,16 @@ export default function Page() {
           <div className="fixed right-5 top-32 z-10 mb-5 rounded-lg bg-accent px-2 py-1 text-sm text-muted-foreground sm:top-20">
             {saveStatus}
           </div>
-          <input
-            className="w-full bg-background py-8 text-center text-6xl focus:outline-none"
-            defaultValue={title}
-            placeholder="Title"
-            onChange={(e) => {
-              setSaveStatus("Unsaved");
-              setTitle(e.target.value);
-            }}
-          ></input>
-          <EditorRoot>
-            {content ? (
-              <EditorContent
-                extensions={extensions}
-                content={content}
-                className="relative min-h-[500px] w-full max-w-screen-lg border-muted bg-background sm:mb-[calc(20vh)] sm:rounded-lg sm:border sm:shadow-lg"
-                editorProps={{
-                  ...defaultEditorProps,
-                  attributes: {
-                    class: `prose-lg prose-stone dark:prose-invert prose-headings:font-title font-default focus:outline-none max-w-full`,
-                  },
-                }}
-                onUpdate={({ editor }) => {
-                  debouncedUpdates(editor);
-                  setSaveStatus("Unsaved");
-                }}
-                slotAfter={<ImageResizer />}
-              >
-                <EditorCommand className="z-50 h-auto max-h-[330px]  w-72 overflow-y-auto rounded-md border border-muted bg-background px-1 py-2 shadow-md transition-all">
-                  <EditorCommandEmpty className="px-2 text-muted-foreground">
-                    No results
-                  </EditorCommandEmpty>
-                  {suggestionItems.map((item) => (
-                    <EditorCommandItem
-                      value={item.title}
-                      onCommand={(val) => item.command(val)}
-                      className={`flex w-full items-center space-x-2 rounded-md px-2 py-1 text-left text-sm hover:bg-accent aria-selected:bg-accent `}
-                      key={item.title}
-                    >
-                      <div className="flex h-10 w-10 items-center justify-center rounded-md border border-muted bg-background">
-                        {item.icon}
-                      </div>
-                      <div>
-                        <p className="font-medium">{item.title}</p>
-                        <p className="text-xs text-muted-foreground">
-                          {item.description}
-                        </p>
-                      </div>
-                    </EditorCommandItem>
-                  ))}
-                </EditorCommand>
-
-                <EditorBubble
-                  tippyOptions={{
-                    placement: openAI ? "bottom-start" : "top",
-                    onHidden: () => {
-                      setOpenAI(false);
-                    },
-                  }}
-                  className="flex w-fit max-w-[90vw] overflow-hidden rounded border border-muted bg-background shadow-xl"
-                >
-                  {openAI ? (
-                    <AISelector open={openAI} onOpenChange={setOpenAI} />
-                  ) : (
-                    <>
-                      <NodeSelector
-                        open={openNode}
-                        onOpenChange={setOpenNode}
-                      />
-                      <Separator orientation="vertical" />
-
-                      <LinkSelector
-                        open={openLink}
-                        onOpenChange={setOpenLink}
-                      />
-
-                      <Separator orientation="vertical" />
-
-                      <TextButtons />
-                      <Separator orientation="vertical" />
-
-                      <ColorSelector
-                        open={openColor}
-                        onOpenChange={setOpenColor}
-                      />
-                    </>
-                  )}
-                </EditorBubble>
-              </EditorContent>
-            ) : (
-              <LoadingCircle />
-            )}
-          </EditorRoot>
+          <TitleEditor
+            title={title}
+            setTitle={setTitle}
+            setSaveStatus={setSaveStatus}
+          />
+          <CustomEditor
+            content={content}
+            debouncedUpdates={debouncedUpdates}
+            setSaveStatus={setSaveStatus}
+          />
         </div>
       )}
       <IssueComments />
